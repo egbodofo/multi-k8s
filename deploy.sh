3ec17d82a29fee24b32a2301307e6eeeb051658b
egbodofo <oluwafabyegbodofo@gmail.com>
2019-10-24 16:04:06 +0100

    Modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,12 @@ docker push egbodofo/multi-client:$SHA
 docker push egbodofo/multi-server:$SHA
 docker push egbodofo/multi-worker:$SHA
 
-kubectl apply -f k8s
+kubectl delete -f k8s
 
-echo "running..."
 
-kubectl set image deployments/server-deployment server=egbodofo/multi-server:$SHA
-kubectl set image deployments/client-deployment client=egbodofo/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=egbodofo/multi-worker:$SHA
+# kubectl set image deployments/server-deployment server=egbodofo/multi-server:$SHA
+# kubectl set image deployments/client-deployment client=egbodofo/multi-client:$SHA
+# kubectl set image deployments/worker-deployment worker=egbodofo/multi-worker:$SHA
 
 
 # Other Userful Commands
